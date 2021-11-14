252bfdcd35a7e6470811515b644bca99c9163d34
Amol11 <amolsinghaniya@gmail.com>
2021-11-14 19:58:34 +0530

    UI enhancements

--- public/js/home.js
+++ public/js/home.js
@@ -3,51 +3,61 @@ var pcGames= [
         tag: 'Batman: Arkham Origins',
         fileName: 'batman ao',
         url: 'https://www.youtube.com/watch?v=QqDWx8kfoXo',
+        year: '2013'
     },
     {
         tag: 'Watch_Dogs',
         fileName: 'Watch Dogs',
         url: 'https://www.youtube.com/watch?v=PFko4Kut39s',
+        year: '2014'
     },
     {
         tag: 'Battlefield 4',
         fileName: 'BF 4',
         url: 'https://www.youtube.com/watch?v=kGuncbV7oIU',
+        year: '2013'
     },
     {
         tag: "Sid Meier's: Civilization VI",
         fileName: 'civi 6',
         url: 'https://www.youtube.com/watch?v=5KdE0p2joJw',
+        year: '2016'
     },
     {
         tag: "Deus Ex: Human Revolution",
         fileName: 'deus ex HR',
         url: 'https://www.youtube.com/watch?v=WAbrjtFbVQ4',
+        year: '2011'
     },
     {
         tag: "Tomb Raider",
         fileName: 'TR 2013',
         url: 'https://www.youtube.com/watch?v=3VmFhs26U2E',
+        year: '2013'
     },
     {
         tag: "Far Cry Primal",
         fileName: 'FC primal',
         url: 'https://www.youtube.com/watch?v=LJ2iH57Fs3M',
+        year: '2016'
     },
     {
         tag: "Mad Max",
         fileName: 'mad max',
         url: 'https://www.youtube.com/watch?v=PUdHYtkDBa4',
+        year: '2015'
     },
     {
         tag: "F.E.A.R. 2: Project Origin",
         fileName: 'FEAR 2',
         url: 'https://www.youtube.com/watch?v=kBTnDwW3i48',
+        year: '2009'
     },
     {
         tag: "The Walking Dead",
         fileName: 'Walking dead',
         url: 'https://www.youtube.com/watch?v=P9G24VZwW5E',
+        year: '2012'
     },
 ];
 
